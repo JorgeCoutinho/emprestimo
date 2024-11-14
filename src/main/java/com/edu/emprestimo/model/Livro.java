c0665ac8c5dfff0791c4fc734d4b6f6f5999ff49
Jorge Mikael <jmca.lic19@uea.edu.br>
2024-11-13 23:16:00 -0300

    Terminando de implementar os modelos

--- src/main/java/com/edu/emprestimo/model/Livro.java
+++ src/main/java/com/edu/emprestimo/model/Livro.java
@@ -1,5 +1,91 @@
 package com.edu.emprestimo.model;
+import java.util.Date;
 
 public class Livro {
+
+    private Long id;
+    private String titulo;
+    private String autor;
+    private Date anoPublicacao;
+    private String editora;
+    private Long qtdDisponivel;
+
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Date getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Date anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public Long getQtdDisponivel() {
+        return qtdDisponivel;
+    }
+
+    public void setQtdDisponivel(Long qtdDisponivel) {
+        this.qtdDisponivel = qtdDisponivel;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
 
 }
